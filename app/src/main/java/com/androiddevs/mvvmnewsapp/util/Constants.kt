bf4c596cfceb919ef8bf2c8fdedfc7b490401fe7
Rishab Prasad <reshab.prasad@gmail.com>
2021-01-01 23:54:26 +0530

    - Retrofit setup - DAO Room setup for Articles

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "cbfadba6ecce4362a725416f61ae34b1"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
